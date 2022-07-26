2b7347ec9744f0c611fa17e0cfea9ca0d50281aa
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-26 20:59:28 +0100

    Update stocks url

--- src/apis/stocksApi.js
+++ src/apis/stocksApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const stocksUrl = 'https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=61d3e77e74207aa24ad5a8c19e3746ab';
+const stocksUrl = 'https://financialmodelingprep.com/api/v3/actives?apikey=61d3e77e74207aa24ad5a8c19e3746ab';
 
 const getStocksData = async () => {
   try {
