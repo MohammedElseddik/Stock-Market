fa8c2ba00593d43fa69be00205ba7b59238bf458
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-27 15:40:43 +0100

    Fetch company info data

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import stocksReducer from '../features/stocksSlice';
+import companyInfoReducer from '../features/companyInfoSlice';
 
 const reducer = combineReducers({
   stocks: stocksReducer,
+  companyInfo: companyInfoReducer,
 });
 
 const store = configureStore({ reducer });
