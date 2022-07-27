ab412bfd5e3f605f12c0b4d863f73b2b15ed6070
Mohammed Elseddik Rebbouh <rebbouh.mohammed.elseddik@gmail.com>
2022-07-27 15:48:33 +0100

    Fix linter errors

--- src/redux/effects/companyInfoEffects.js
+++ src/redux/effects/companyInfoEffects.js
@@ -1,17 +1,16 @@
-const companyInfoEffectCleanData = ({ payload }) =>
-  payload.map((company) => ({
-    image: company.image,
-    companyName: company.companyName,
-    ceo: company.ceo,
-    address: company.address,
-    zip: company.zip,
-    marketCap: company.mktCap,
-    ticker: company.symbol,
-    currency: company.currency,
-    StockPrice: company.price,
-    changes: company.changes,
-    description: company.description,
-    website: company.website,
-  }));
+const companyInfoEffectCleanData = ({ payload }) => payload.map((company) => ({
+  image: company.image,
+  companyName: company.companyName,
+  ceo: company.ceo,
+  address: company.address,
+  zip: company.zip,
+  marketCap: company.mktCap,
+  ticker: company.symbol,
+  currency: company.currency,
+  StockPrice: company.price,
+  changes: company.changes,
+  description: company.description,
+  website: company.website,
+}));
 
 export default companyInfoEffectCleanData;
